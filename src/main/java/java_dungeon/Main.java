dd2a1a45df54317bb689079461b890813f8b8e17
Marcus Smith <smitma16@pfw.edu>
2025-03-28 17:53:49 -0400

    Added basic tilemap rendering and a camera

--- src/main/java/java_dungeon/Main.java
+++ src/main/java/java_dungeon/Main.java
@@ -1,9 +1,10 @@
 package java_dungeon;
 
+import java_dungeon.map.GameMap;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -11,50 +12,105 @@ import javafx.stage.Stage;
 import java_dungeon.gui.AutoScalingCanvas;
 
 public class Main extends Application {
-    private static final int width = 128;
-    private static final int height = 128;
+    // SNES rendering size
+    private static final int SCREEN_WIDTH = 256;
+    private static final int SCREEN_HEIGHT = 224;
+
+    private GameMap map;
+    private AutoScalingCanvas canvas;
+    private GraphicsContext ctx;
+
+    private double ppu = 16; // Pixels per unit, 16 pixels per game unit
+    private double camX;
+    private double camY;
+
+    public Main() {
+        this.map = new GameMap();
+        camX = 0.0;
+        camY = 0.5;
+    }
 
     @Override
     public void start(Stage stage) {
         StackPane root = new StackPane();
-        AutoScalingCanvas canvas = new AutoScalingCanvas(width, height);
-
+        canvas = new AutoScalingCanvas(SCREEN_WIDTH, SCREEN_HEIGHT);
         root.getChildren().add(canvas);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        gc.setImageSmoothing(false);
-        Image img = new Image("dungeon-tiles.png");
-        redraw(img, gc, canvas.scalingProperty().get());
+        ctx = canvas.getGraphicsContext2D();
+        ctx.setImageSmoothing(false);
 
-        canvas.scalingProperty().addListener((o, oldScale, newScale) -> redraw(img, gc, newScale.doubleValue()));
+        renderGame();
 
-        Scene scene = new Scene(root, width * 5, height * 5);
-        scene.setFill(Color.BLACK);
+        // Redraw the game if the canvas is resized (window resizing)
+        canvas.scalingProperty().addListener((o, oldScale, newScale) -> renderGame());
+
+        // Basic update loop (Moves the camera 1 unit to the left every second)
+        AnimationTimer animator = new AnimationTimer() {
+            @Override
+            public void handle(long currentTime) {
+                camX -= 1.0/60.0;
+                renderGame();
+            }
+        };
+        animator.start();
+
+        // Start the window at 3x size
+        Scene scene = new Scene(root, SCREEN_WIDTH * 3, SCREEN_HEIGHT * 3);
+        scene.setFill(Color.BLACK); // Black background color
         stage.setScene(scene);
         stage.setTitle("Java Dungeon");
         stage.show();
     }
 
-    private void redraw(Image img, GraphicsContext gc, double scaling) {
-        gc.save();
-        gc.scale(scaling, scaling);
+    private void renderGame() {
+        // Clear the screen
+        ctx.setFill(Color.BLACK);
+        ctx.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-        gc.setFill(Color.DARKGREEN);
-        gc.fillRect(0, 0, 256, 256);
+        // Apply camera transformations (including render scaling)
+        double renderScale = canvas.scalingProperty().get();
+        ctx.save();
+        ctx.scale(renderScale, renderScale);
+        ctx.translate(-camX * ppu, -camY * ppu);
 
-        gc.drawImage(img, 0, 0);
+        // Draw the tilemap
+        renderTiles();
 
-        gc.translate(72, 72);
-        gc.rotate(45);
-        gc.setFill(Color.BLUE);
-        gc.fillRect(-8, -8, 16, 16);
-        gc.rotate(-45);
-        gc.translate(-72, -72);
+        ctx.restore();
+    }
 
-        gc.setFill(Color.RED);
-        gc.fillRect(32, 64, 16, 16);
+    private void renderTiles() {
+        int[][] renderMap = map.getRenderMap();
 
-        gc.restore();
+        // Assume the map width and height from the first row
+        int w = renderMap[0].length;
+        int h = renderMap.length;
+
+        // Start drawing from the map position
+        ctx.translate(map.getOriginX() * ppu, map.getOriginY() * ppu);
+
+        // Only draw the visible tiles of the tilemap
+        // Upper left corner of the screen in tile coordinates (bounded by the tilemap)
+        int startX = (int)Math.max(Math.floor(((camX - map.getOriginX()) * ppu) / GameMap.TILE_SIZE), 0);
+        int startY = (int)Math.max(Math.floor(((camY - map.getOriginY()) * ppu) / GameMap.TILE_SIZE), 0);
+
+        // Bottom right corner of the screen in tilemap coordinates (bounded by the tilemap)
+        int endX = (int)Math.min(Math.floor(((camX - map.getOriginX()) * ppu + SCREEN_WIDTH) / GameMap.TILE_SIZE) + 1, w);
+        int endY = (int)Math.min(Math.floor(((camY - map.getOriginY()) * ppu + SCREEN_HEIGHT) / GameMap.TILE_SIZE) + 1, h);
+
+        // Draw the tiles
+        for (int y = startY; y < endY; y++) {
+            for (int x = startX; x < endX; x++) {
+                ctx.drawImage(
+                    map.getTilesetImage(),
+                    map.getTileset()[renderMap[y][x]].x, map.getTileset()[renderMap[y][x]].y, GameMap.TILE_SIZE, GameMap.TILE_SIZE,
+                    x * GameMap.TILE_SIZE, y * GameMap.TILE_SIZE, GameMap.TILE_SIZE, GameMap.TILE_SIZE
+                );
+            }
+        }
+
+        // Reset the map translation
+        ctx.translate(-map.getOriginX() * ppu, map.getOriginY() * ppu);
     }
 
     public static void main(String[] args) {
--- src/main/java/java_dungeon/map/GameMap.java
+++ src/main/java/java_dungeon/map/GameMap.java
@@ -1,5 +1,122 @@
 package java_dungeon.map;
 
-public class GameMap {
+import javafx.scene.image.Image;
 
+import java.util.Random;
+
+public class GameMap {
+    public static int TILE_SIZE = 16;
+
+    private double originX;
+    private double originY;
+
+    public static class Tile {
+        public final int x, y;
+
+        public Tile(int xIndex, int yIndex) {
+            this.x = xIndex * TILE_SIZE;
+            this.y = yIndex * TILE_SIZE;
+        }
+    }
+
+    private Tile[] tileset;
+    private Image tilesetImage;
+
+    private int[][] renderMap;
+
+    public GameMap() {
+        loadTileset();
+        generateTilemap();
+
+        this.originX = 0.0;
+        this.originY = 7.0;
+    }
+
+    public Image getTilesetImage() {
+        return tilesetImage;
+    }
+
+    public Tile[] getTileset() {
+        return tileset;
+    }
+
+    public int[][] getRenderMap() {
+        return renderMap;
+    }
+
+    public double getOriginX() {
+        return originX;
+    }
+    public double getOriginY() {
+        return originY;
+    }
+
+    public void setOriginX(double originX) {
+        this.originX = originX;
+    }
+    public void setOriginY(double originY) {
+        this.originY = originY;
+    }
+
+    private void loadTileset() {
+        // The tileset is 5x3 tiles
+        tilesetImage = new Image("dungeon-tiles.png");
+
+        int tilesPerRow = 5;
+        int tilesPerCol = 3;
+        tileset = new Tile[tilesPerRow * tilesPerCol];
+
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 5; x++) {
+                tileset[y * tilesPerRow + x] = new Tile(x, y);
+            }
+        }
+    }
+
+    private void generateTilemap() {
+        // ToDo: Add better dungeon generation
+        // 16x14 tiles tilemap (screen size)
+        int w = 16;
+        int h = 14;
+
+        renderMap = new int[h][w];
+
+        // Outer wall
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                int tile = 6;
+
+                if (x == 0 && y == 0) {
+                    tile = 0;
+                }
+                else if (x == (w - 1) && y == 0) {
+                    tile = 2;
+                }
+                else if (x == 0 && y == (h - 1)) {
+                    tile = 10;
+                }
+                else if (x == (w - 1) && y == (h - 1)) {
+                    tile = 12;
+                }
+                else if (x == 0) {
+                    tile = 5;
+                }
+                else if (x == (w - 1)) {
+                    tile = 7;
+                }
+                else if (y == 0) {
+                    tile = 1;
+                }
+                else if (y == (h - 1)) {
+                    tile = 11;
+                }
+                renderMap[y][x] = tile;
+            }
+        }
+        // Random blocks
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            this.renderMap[rand.nextInt(0, h)][rand.nextInt(0, w)] = 14;
+        }
+    }
 }
